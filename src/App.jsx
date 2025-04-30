6d205eb72fd3b01d5df5d6a4a0900c4a85615647
firas hamila <firas.hm.1996@gmail.com>
2025-04-30 11:00:18 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Contact from "./screens/Contact";
 import Products from "./screens/Products";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import NotFound from "./screens/NotFound";
+import ProductDetails from "./screens/ProductDetails";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Products" element={<Products />} />
+        <Route path="/Products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
--- src/screens/Products.jsx
+++ src/screens/Products.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
     <div>
       <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/Products/1">Products 1</Link>
+        </li>
+        <li>
+          <Link to="/Products/2">Products 2</Link>
+        </li>
+        <li>
+          <Link to="/Products/3">Products 3</Link>
+        </li>
+      </ul>
     </div>
   );
 };
